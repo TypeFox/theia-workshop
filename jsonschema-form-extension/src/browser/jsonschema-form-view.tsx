47b151b5f988bfcd13d075194769201660060f76
Anton Kosiakov <anton.kosyakov@typefox.io>
2018-06-06 20:56:21 +0200

    initial commit

---
+++ jsonschema-form-extension/src/browser/jsonschema-form-view.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+
+export interface Greeting {
+    name: string
+    at: string
+}
+export class GreetingView extends React.Component<Greeting> {
+    render(): JSX.Element {
+        return <h1>Hello {this.props.name} at {this.props.at}!</h1>;
+    }
+}
+
+export class JsonschemaFormView extends React.Component<{}, Greeting> {
+
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            name: 'World',
+            at: 'EclipseCon France 2018'
+        }
+    }
+
+    render(): JSX.Element {
+        return <React.Fragment>
+            <GreetingView name={this.state.name} at={this.state.at} />
+            Greet <input value={this.state.name} onChange={this.updateName} /> at <input value={this.state.at} onChange={this.updateAt} />
+        </React.Fragment>;
+    }
+
+    protected updateName = (e: React.ChangeEvent<HTMLInputElement>) => this.setState({
+        name: e.currentTarget.value
+    });
+
+    protected updateAt = (e: React.ChangeEvent<HTMLInputElement>) => this.setState({
+        at: e.currentTarget.value
+    });
+
+}
+
