0ae6883a40fbb39276a721882ba63e1d587803f3
Anton Kosyakov <anton.kosyakov@typefox.io>
2019-10-17 11:10:01 +0000

    udpate to latest theia

--- jsonschema-form-extension/src/browser/jsonschema-form-view.tsx
+++ jsonschema-form-extension/src/browser/jsonschema-form-view.tsx
@@ -19,6 +19,11 @@ export class JsonschemaFormView extends React.Component<{}, Greeting> {
     }
 
     render(): JSX.Element {
+        // TODO: render a place of greeting:
+        // - Add a new property `at` to `Greeting` of `string` type to define a place of greeting state.
+        // - Initialize the default place of greeting as `Theia Workshop` in the constructor.
+        // - Add a new property `updateAt` to update the place of greeting state. It should be implemented after `updateName` property.
+        // - Render another input field for the place of greeting after the input field for name. Separate them by ` at ` string.
         return <React.Fragment>
             <GreetingView name={this.state.name} />
             Greet <input value={this.state.name} onChange={this.updateName} />
